2d9f25b4f16987dfbd5f1cce900dd089e6b4b3e9
Dane Walker <dane@danecwalker.com>
2022-02-17 20:18:29 +1000

    First Commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import HomeView from './src/views/HomeView';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <HomeView/>
+    </SafeAreaProvider>
   );
 }
 
